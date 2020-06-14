6125467b50b8cd69a6315f60690b5c6ec35ab4f6
seraht <serah1995@hotmail.fr>
2020-06-14 15:37:43 +0300

    After change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 
 if __name__ == "__main__":
     num = sys.argv[1]
-    for i in range(num):
+    for i in range(int(num)):
         print(str(i) + " " + str(i+1))
         sleep(1)
 
