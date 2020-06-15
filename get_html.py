d1884f0a9e2146d4726e362e3e2384d4ed0fabb1
seraht <serah1995@hotmail.fr>
2020-06-15 12:22:57 +0300

    change num

---
+++ get_html.py
@@ -0,0 +1,16 @@
+"""gets a list of comma delimited urls as an input from a Jenkins job
+and, loops over each url and prints the HTML of that url."""
+import sys
+import urllib
+
+if __name__ == "__main__":
+    urls = sys.argv[1]
+    for u in urls.split(","):
+        print(u)
+        try:
+            req = urllib.request.urlopen(u).read()
+            print(req)
+        except ValueError:
+            print("There is an error when parsing html")
+            sys.exit(0)
+
